88da516ef5cc913bf63b2e1b9ba0078fe5f97491
neocim <frag1337n@gmail.com>
2026-02-11 13:12:45 +0300

    Add `Goal` entity

---
+++ src/studytracker/entities/goal.py
@@ -0,0 +1,27 @@
+from datetime import date
+from uuid import UUID
+
+from studytracker.entities.common.entity import Entity
+
+
+class Goal(Entity[UUID]):
+    def __init__(
+        self,
+        entity_id: UUID,
+        parent_id: UUID,
+        user_id: UUID,
+        period_start: date,
+        period_end: date,
+        name: str,
+        description: str | None = None,
+        is_success: bool | None = None,
+    ) -> None:
+        super().__init__(entity_id=entity_id)
+
+        self._parent_id = parent_id
+        self._user_id = user_id
+        self._period_start = period_start
+        self._period_end = period_end
+        self._name = name
+        self._description = description
+        self._is_success = is_success
