7e40fbe01c4701cc3201d6022ef65a3bc9f3a468
neocim <frag1337n@gmail.com>
2026-02-23 15:35:49 +0300

    Add `delete_goal` to di

--- src/studytracker/bootstrap/di/providers/application.py
+++ src/studytracker/bootstrap/di/providers/application.py
@@ -3,11 +3,12 @@ from bazario.asyncio.resolvers.dishka import DishkaResolver
 from dishka import Provider, Scope, WithParents, provide, provide_all
 
 from studytracker.application.commands.create_goal import CreateGoalHandler, CreateGoalRequest
+from studytracker.application.commands.delete_goal import DeleteGoalHandler, DeleteGoalRequest
 from studytracker.application.queries.get_goal import GetGoalHandler, GetGoalRequest
 
 
 class ApplicationProvider(Provider):
-    handlers = provide_all(CreateGoalHandler, GetGoalHandler, scope=Scope.REQUEST)
+    handlers = provide_all(CreateGoalHandler, GetGoalHandler, DeleteGoalHandler, scope=Scope.REQUEST)
 
     resolver = provide(WithParents[DishkaResolver], scope=Scope.REQUEST)
     dispatcher = provide(WithParents[Dispatcher], scope=Scope.REQUEST)
@@ -18,5 +19,6 @@ class ApplicationProvider(Provider):
 
         registry.add_request_handler(CreateGoalRequest, CreateGoalHandler)
         registry.add_request_handler(GetGoalRequest, GetGoalHandler)
+        registry.add_request_handler(DeleteGoalRequest, DeleteGoalHandler)
 
         return registry
